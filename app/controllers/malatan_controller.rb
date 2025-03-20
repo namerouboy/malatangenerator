38607fe83fa3de61a28f385fe4aaeb51e3779166
namerouboy <166996344+namerouboy@users.noreply.github.com>
2025-03-20 13:45:25 +0000

    合計値出力機能を実装

--- app/controllers/malatan_controller.rb
+++ app/controllers/malatan_controller.rb
@@ -1,4 +1,11 @@
 class MalatanController < ApplicationController
   def index
+    @syokuzai = Syokuzai.all
   end
+
+  def search
+    @syokuzai = Syokuzai.where(name: params[:syokuzai.name])
+    render "/malatan/result"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "malatan#index"
+
+  get "search" => "malatan#search"
+
 end
