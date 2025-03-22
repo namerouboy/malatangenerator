463e6f75bd4a16a42279af28bc5f31b8c21689fb
namerouboy <166996344+namerouboy@users.noreply.github.com>
2025-03-22 12:34:16 +0000

    cssを適用

--- app/controllers/malatan_controller.rb
+++ app/controllers/malatan_controller.rb
@@ -1,10 +1,17 @@
 class MalatanController < ApplicationController
   def index
-    @syokuzai = Syokuzai.all
+    @soup = Syokuzai.where(category: "soup")
+    @yasai = Syokuzai.where(category: "vegetables")
+    @men = Syokuzai.where(category: "men")
+    @nerimono = Syokuzai.where(category: "nerimono")
+    @niku = Syokuzai.where(category: "meats")
+    @sakana = Syokuzai.where(category: "seafoods")
+    @sonota = Syokuzai.where(category: "sonota")
   end
 
   def search
     @syokuzai = Syokuzai.where(name: params[:syokuzai.name])
+    @kazu = params[:kazu]
     render "/malatan/result"
   end
 
