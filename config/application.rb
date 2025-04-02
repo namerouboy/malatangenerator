a6b5f6b6c3c671d47871c5a8fff5c8f26cc311ee
namerouboy <166996344+namerouboy@users.noreply.github.com>
2025-04-02 11:44:39 +0000

    yarnを使わない

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module CodespacesTryRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # JavaScript のビルドを無効化（yarn を使わずに assets:precompile を成功させる）
+    config.assets.js_compressor = nil
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
