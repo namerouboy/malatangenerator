0e0c71ef5f3c3931f29564ca3d28fadd620f018b
namerouboy <166996344+namerouboy@users.noreply.github.com>
2025-04-24 03:50:48 +0000

    デプロイごとにseedが重複しないように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#   
+
+Suuryou.destroy_all
+Syokuzai.destroy_all
 
 for i in 0..10
   Suuryou.create(suuryou: i)
